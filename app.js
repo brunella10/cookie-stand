f78260385220d018ce3c2c7ca1c7abd26442e5d9
Gabriela Diez <gaby1010@gmail.com>
2017-06-12 17:55:07 -0700

    Stores the min/max hourly customers, and the average cookies per customer, in object properties

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//1st shop 1st and Pike	23	65	6.3
+var pikeShop = {
+  minCustomerPerHour: 23,
+  maxCustomerPerHour: 65,
+  avgCookieSalePerHour: 6.3,
+};
+
+// 2nd shop SeaTac Airport	3	24	1.2
+var SeaTac = {
+  minCustomerPerHour: 3,
+  maxCustomerPerHour: 24,
+  avgCookieSalePerHour: 1.2,
+};
+
+//Seattle Center	11	38	3.7
+var downTown = {
+  minCustomerPerHour: 11,
+  maxCustomerPerHour: 38,
+  avgCookieSalePerHour: 3.7,
+};
+
+//Capitol Hill	20	38	2.3
+var capitolHill = {
+  minCustomerPerHour: 20,
+  maxCustomerPerHour: 38,
+  avgCookieSalePerHour: 2.3,
+};
+
+//Alki	2	16	4.6
+var alkiShop = {
+  minCustomerPerHour: 2,
+  maxCustomerPerHour: 16,
+  avgCookieSalePerHour: 4.6,
+};
+console.log(pikeShop, SeaTac, downTown, capitolHill, alkiShop);
