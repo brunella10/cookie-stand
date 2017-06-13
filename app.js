bdc0d7ec5665da16130bb799f8ca9337c1db160e
Gabriela Diez <gaby1010@gmail.com>
2017-06-12 18:24:34 -0700

    create function costumerPerHour and object literal with a random number

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var pikeShop = {
   minCustomerPerHour: 23,
   maxCustomerPerHour: 65,
   avgCookieSalePerHour: 6.3,
+  numCustomerPerHour: function() {
+    return Math.random() * (this.maxCustomerPerHour - this.minCustomerPerHour) + this.minCustomerPerHour;
+  }
 };
 
 // 2nd shop SeaTac Airport	3	24	1.2
@@ -12,6 +15,9 @@ var SeaTac = {
   minCustomerPerHour: 3,
   maxCustomerPerHour: 24,
   avgCookieSalePerHour: 1.2,
+  numCustomerPerHour: function() {
+    return Math.random() * (this.maxCustomerPerHour - this.minCustomerPerHour) + this.minCustomerPerHour;
+  }
 };
 
 //Seattle Center	11	38	3.7
@@ -19,6 +25,9 @@ var downTown = {
   minCustomerPerHour: 11,
   maxCustomerPerHour: 38,
   avgCookieSalePerHour: 3.7,
+  numCustomerPerHour: function() {
+    return Math.random() * (this.maxCustomerPerHour - this.minCustomerPerHour) + this.minCustomerPerHour;
+  }
 };
 
 //Capitol Hill	20	38	2.3
@@ -26,6 +35,9 @@ var capitolHill = {
   minCustomerPerHour: 20,
   maxCustomerPerHour: 38,
   avgCookieSalePerHour: 2.3,
+  numCustomerPerHour: function() {
+    return Math.random() * (this.maxCustomerPerHour - this.minCustomerPerHour) + this.minCustomerPerHour;
+  }
 };
 
 //Alki	2	16	4.6
@@ -33,5 +45,7 @@ var alkiShop = {
   minCustomerPerHour: 2,
   maxCustomerPerHour: 16,
   avgCookieSalePerHour: 4.6,
+  numCustomerPerHour: function() {
+    return Math.random() * (this.maxCustomerPerHour - this.minCustomerPerHour) + this.minCustomerPerHour;
+  }
 };
-console.log(pikeShop, SeaTac, downTown, capitolHill, alkiShop);
